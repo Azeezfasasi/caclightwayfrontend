afb21a1e18004cca1ef838722561b2197e4b2a84
azeezfasasi <fas.azeez@gmail.com>
2025-07-21 17:20:30 +0100

    yes

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -3,3 +3,6 @@
 export const BACKEND_URL = 'https://cac-lightway-backend.onrender.com';
 export const API_BASE_URL = 'https://cac-lightway-backend.onrender.com/api';
 
+// export const API_BASE_URL = process.env.NODE_ENV === 'production'
+//   ? 'https://cac-lightway-backend.onrender.com/api' // Hosted backend
+//   : 'http://localhost:5000/api'; 
