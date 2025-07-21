a5513e75e0b781e994abf1229428b891059ccb5b
azeezfasasi <fas.azeez@gmail.com>
2025-07-21 16:36:47 +0100

    Updated

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,2 +1,5 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
-export const BACKEND_URL = 'http://localhost:5000';
+// export const API_BASE_URL = 'http://localhost:5000/api';
+// export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = 'https://cac-lightway-backend.onrender.com';
+export const API_BASE_URL = 'https://cac-lightway-backend.onrender.com';
+
